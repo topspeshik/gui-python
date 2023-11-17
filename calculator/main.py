baeee9213d889a104c0d06c2d0e7347bdc85a450
Daniel <speshik222@gmail.com>
2023-11-17 22:33:56 +0800

    fix: keyboard input

--- calculator/main.py
+++ calculator/main.py
@@ -1,12 +1,30 @@
 import tkinter as tk
 from ViewModel import ViewModel
 
+def validateNumbers(P):
+        if P == '':
+            return True
+        elif P in ['/', '*', '-', '+','1/x','x²','√','⌫','CE', 'C','±','.','%']:
+            return True
+        elif P.isdigit():
+            return True
+        else:
+            try:
+                float(P)
+                return  True
+            except:
+                return False
+
+
 window = tk.Tk()
 window.title("Calculator")
 window.geometry('360x510')
 
+reg = window.register(validateNumbers)
+
 startValue = tk.StringVar(value='0')
 input_entry = tk.Entry(window, bg="#ebecef", bd=0, textvariable=startValue, font=('Segoe UI', 32, 'bold'), justify='right')
+input_entry.config(validate="key", validatecommand =(reg, '%P'))
 input_entry.grid(column=0, row=1, columnspan=4, sticky='wens', padx =8)
 
 history_lbl = tk.Label(window, bg="#ebecef", text="", font=('Segoe UI', 12), anchor='e')
@@ -32,7 +50,7 @@ for i in range(2, 8):
                                                                                padx=2,
                                                                                pady=2)
         else:
-            tk.Button(window, text=btn_lbl, bd=0, bg="#f8f8f8", font=('Segoe UI Semibold', 12),
+            tk.Button(window, text=btn_lbl, bd=0, bg="#D2ECEC", font=('Segoe UI Semibold', 12),
                       command=lambda l=btn_lbl: viewModel.parseButton(l)).grid(column=j,
                                                                                row=i,
                                                                                sticky='wens',
